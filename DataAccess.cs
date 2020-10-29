7f4b598712bb93ac4758a1ae478583b494b911ce
Paul Glavich <glav@theglavs.com>
2020-10-29 17:10:25 +1100

    Adding actual usage of SQL Conn string to trigger alert

--- DataAccess.cs
+++ DataAccess.cs
@@ -1,23 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace CodeQLAlertTrigger
 {
     public class DataAccess
     {
-        private Dictionary<string,string> _users = new Dictionary<string, string>();
-        private const string SqlConnectionString = "Server=localhost;Database=SecurityDb;User Id=generaluser;Password=somepassword;";
+        private Dictionary<string, string> _users = new Dictionary<string, string>();
+        private const string SqlConnectionString = "Server=localhost;Database=SecurityDb;User Id=sa;Password=MyP@ssw0rd!;";
 
         public DataAccess()
         {
-            _users.Add("user1","password1");
-            _users.Add("user2","password2");
-            _users.Add("user3","password3");
-            _users.Add("user4","password4");
         }
 
         public bool IsValidUser(string username, string password)
         {
+            PopulateUsers();
             // Should access the database but we wont
             if (!_users.ContainsKey(username))
             {
@@ -25,6 +23,33 @@ namespace CodeQLAlertTrigger
             }
 
             return _users[username] == password;
+        }
+
+        private void PopulateUsers()
+        {
+            try
+            {
+                using (var conn = new SqlConnection(SqlConnectionString))
+                using (var cmd = conn.CreateCommand())
+                {
+                    conn.Open();
+                    cmd.CommandText = "select username,password from users";
+                    var rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        _users.Add(rdr.GetString(0), rdr.GetString(1));
+
+                    }
+                }
+            } catch
+            {
+                // Accept DB failure, populate pre-filled
+                _users.Add("user1", "password1");
+                _users.Add("user2", "password2");
+                _users.Add("user3", "password3");
+                _users.Add("user4", "password4");
+
+            }
         }
     }
 }
